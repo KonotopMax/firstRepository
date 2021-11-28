694ed746ef101945c73d7ee399c3c326d47dc526
Konotop Maxim <konotop.m.i@gmail.com>
2021-11-28 23:09:40 +0300

    Добавил ветку lesson02 с простым заданием

--- script.js
+++ script.js
@@ -1,10 +1,22 @@
-let title;
-let screens;
-let screenPrice;
-let rollback;
-let fullPrice;
-let adaptive;
+let title = "lesson02";
+let screens = "Simple, Complex, Interactive";
+let screenPrice = 10;
+let rollback = 99;
+let fullPrice = 999999999;
+let adaptive = true;
 
+console.log(typeof title);
+console.log(typeof fullPrice);
+console.log(typeof adaptive);
+console.log(screens.length);
 
-console.log("Hello world")
-alert("Мой первый урок")
+let priceOfScreens = screenPrice + "$";
+console.log(priceOfScreens);
+
+let allFullPrice = fullPrice + "$";
+console.log(allFullPrice);
+
+let str = screens.toLowerCase();
+console.log(str.split(", "));
+
+console.log(fullPrice * (rollback / 100));
