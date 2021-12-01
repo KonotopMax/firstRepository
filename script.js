a32cc897043cb61971e2bd888ac904786270dc08
Максим Конотоп <konotop@Mac-mini-Maksim.local>
2021-12-01 10:05:13 +0300

    Исправление ошибок по lesson03

--- script.js
+++ script.js
@@ -1,39 +1,19 @@
-let title = "lesson03";
-let screens = "Simple, Complex, Interactive";
-let screenPrice = 10;
-let rollback = 99;
-let fullPrice = 999999999;
+let rollback = 20;
 let adaptive = true;
 
-console.log(typeof title);
-console.log(typeof fullPrice);
-console.log(typeof adaptive);
-console.log(screens.length);
-
-let priceOfScreens;
-console.log(priceOfScreens + "$");
-
-let allFullPrice;
-console.log(allFullPrice + "$");
-
-let str = screens.toLowerCase();
-console.log(str.split(", "));
-
-console.log(fullPrice * (rollback / 100));
-
-title = prompt("Как называется ваш проект?");
-screens = prompt("Какие типы экранов нужно разработать? \nпример: Простые, Сложные, Интерактивные");
-screenPrice = parseInt(prompt("Сколько будет стоить данная работа? \nпример: 12000"));
+let title = prompt("Как называется ваш проект?");
+let screens = prompt("Какие типы экранов нужно разработать? \nПример: Простые, Сложные, Интерактивные");
+let screenPrice = parseInt(prompt("Сколько будет стоить данная работа? \nПример: 12000"));
 adaptive = confirm("Нужен ли адаптив на сайте?");
 
-let service1 = prompt("Какой дополнительный тип услуги нужен? \nнапример service1, service2");
-let servicePrice1 = parseInt(prompt("Сколько это будет стоить? \nнапример servicePrice1, servicePrice2"));
-let service2 = prompt("Какой дополнительный тип услуги нужен? \nнапример service1, service2");
-let servicePrice2 = parseInt(prompt("Сколько это будет стоить? \nнапример servicePrice1, servicePrice2"));
+let service1 = prompt("Какой дополнительный тип услуги нужен? \nПример: Планшет");
+let servicePrice1 = parseInt(prompt("Сколько это будет стоить? \nПример: 5000"));
+let service2 = prompt("Какой дополнительный тип услуги нужен? \nПример: Планшет");
+let servicePrice2 = parseInt(prompt("Сколько это будет стоить? \nПример: 5000"));
 
-fullPrice = screenPrice + servicePrice1 + servicePrice2;
-l
-servicePercentPrice = Math.ceil(fullPrice - (fullPrice * (rollback / 100)));
+let fullPrice = screenPrice + servicePrice1 + servicePrice2;
+
+let servicePercentPrice = Math.ceil(fullPrice - (fullPrice * (rollback / 100)));
 console.log(servicePercentPrice);
 
 if (fullPrice > 0 && fullPrice < 15000) {
@@ -43,5 +23,5 @@ if (fullPrice > 0 && fullPrice < 15000) {
 } else if (fullPrice >= 30000) {
   console.log("Даем скидку в 10%");
 } else {
-  onsole.log("Скидка не предусмотрена");
+  console.log("Скидка не предусмотрена");
 }
