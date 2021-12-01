6ccbe09feefe50c86fdf27774d993e6a05539968
Максим Конотоп <konotop@Mac-mini-Maksim.local>
2021-12-01 10:50:11 +0300

    Исправление ошибок №2 по lesson03

--- script.js
+++ script.js
@@ -4,12 +4,14 @@ let adaptive = true;
 let title = prompt("Как называется ваш проект?");
 let screens = prompt("Какие типы экранов нужно разработать? \nПример: Простые, Сложные, Интерактивные");
 let screenPrice = parseInt(prompt("Сколько будет стоить данная работа? \nПример: 12000"));
+
 adaptive = confirm("Нужен ли адаптив на сайте?");
 
-let service1 = prompt("Какой дополнительный тип услуги нужен? \nПример: Планшет");
-let servicePrice1 = parseInt(prompt("Сколько это будет стоить? \nПример: 5000"));
-let service2 = prompt("Какой дополнительный тип услуги нужен? \nПример: Планшет");
-let servicePrice2 = parseInt(prompt("Сколько это будет стоить? \nПример: 5000"));
+let service1 = prompt("Какой дополнительный тип услуги нужен?", 'Планшет');
+let servicePrice1 = parseInt(prompt("Сколько это будет стоить?", 5000 ));
+
+let service2 = prompt("Какой дополнительный тип услуги нужен?", 'Планшет');
+let servicePrice2 = parseInt(prompt("Сколько это будет стоить?", 5000 ));
 
 let fullPrice = screenPrice + servicePrice1 + servicePrice2;
 
